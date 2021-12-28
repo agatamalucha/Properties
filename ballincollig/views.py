dcbfb8fe85d856585eb8e9c5d5a55aaf7031cd8d
agatamalucha <agata.malucha@gmail.com>
2021-12-28 18:52:03 +0000

    SQL query

--- ballincollig/views.py
+++ ballincollig/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db import connection
 from django.template.defaulttags import register
 from core.models import BallincolligPropertyModel
 from django.db.models import Sum
@@ -8,8 +9,14 @@ import pandas as pd
 
 @login_required
 def ballincollig_properties(request):
-    properties = BallincolligPropertyModel.objects.all().values()
-    dataset = pd.DataFrame(data=properties)
-    dataset = dataset.drop("id",axis=1)
+
+    query = 'SELECT * FROM core_ballincolligpropertymodel'
+    properties =  BallincolligPropertyModel.objects.raw(query)
+
+    #properties = BallincolligPropertyModel.objects.all().values()
+    # Raw SQL Query
+    #dataset = pd.DataFrame(data=properties)
+    dataset = pd.DataFrame([item.__dict__ for item in properties])
+    dataset = dataset.drop(["id","_state"],axis=1)
     dataset = dataset.to_html(index=False)
     return render(request, "ballincollig-properties.html",{"properties": properties, "dataset": dataset})
