05f8d9b05fd39e492c937b41b39d11574fec02ee
agatamalucha <agata.malucha@gmail.com>
2022-01-07 18:34:52 +0000

    Search box

--- ballincollig/urls.py
+++ ballincollig/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from ballincollig.views import ballincollig_properties
+from ballincollig.views import ballincollig_properties, search_property
 
 app_name = "ballincollig"
 
 urlpatterns = [
 
     path("", ballincollig_properties, name="ballincollig"),
+    path("search_property", search_property, name="search_property" ),
 
 ]
--- ballincollig/views.py
+++ ballincollig/views.py
@@ -9,14 +9,37 @@ import pandas as pd
 
 @login_required
 def ballincollig_properties(request):
-
+    areas = ['Maglin', 'Old Quarter', 'An Caislean','Muskerry Estate','Greenfield',]
     query = 'SELECT * FROM core_ballincolligpropertymodel'
     properties =  BallincolligPropertyModel.objects.raw(query)
 
+
+
+    if len(properties) > 0:
+        # properties = BallincolligPropertyModel.objects.all().values()
+        # Raw SQL Query
+        # dataset = pd.DataFrame(data=properties)
+        dataset = pd.DataFrame([item.__dict__ for item in properties])
+        dataset = dataset.drop(["id", "_state"], axis=1)
+        dataset = dataset.to_html(index=False)
+
+    else:
+        dataset = pd.DataFrame()
+
+    return render(request, "ballincollig-properties.html",{"properties": properties, "dataset": dataset, "areas": areas})
+
+
+
+@login_required
+def search_property(request):
+    areas = ['Maglin', 'Old Quarter', 'An Caislean', 'Muskerry Estate','Greenfield', ]
+    area = request.GET['area']
+    query = f'SELECT * FROM core_ballincolligpropertymodel WHERE area = "{area}" '
+    properties =  BallincolligPropertyModel.objects.raw(query)
     #properties = BallincolligPropertyModel.objects.all().values()
     # Raw SQL Query
     #dataset = pd.DataFrame(data=properties)
     dataset = pd.DataFrame([item.__dict__ for item in properties])
-    dataset = dataset.drop(["id","_state"],axis=1)
+    dataset = dataset.drop(["id", "_state"], axis=1)
     dataset = dataset.to_html(index=False)
-    return render(request, "ballincollig-properties.html",{"properties": properties, "dataset": dataset})
+    return render(request, "ballincollig-properties.html",{"properties": properties, "dataset": dataset,"areas": areas })
