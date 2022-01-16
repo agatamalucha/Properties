dc67129303f32c9d6a4ed577540f31bac27d2c8a
agatamalucha <agata.malucha@gmail.com>
2022-01-16 12:23:05 +0000

    SQl query -search year

--- ballincollig/urls.py
+++ ballincollig/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from ballincollig.views import ballincollig_properties, search_property
+from ballincollig.views import ballincollig_properties, search_property, search_area, ballincollig_year, search_year
 
 app_name = "ballincollig"
 
 urlpatterns = [
 
     path("", ballincollig_properties, name="ballincollig"),
-    path("search_property", search_property, name="search_property" ),
+    path("search-property", search_property, name="search-property"),
+    path("search-area", search_area, name="search-area"),
+    path("ballincollig-year", ballincollig_year, name="ballincollig-year"),
+    path("search-year", search_year, name="search-year"),
 
 ]
--- ballincollig/views.py
+++ ballincollig/views.py
@@ -9,11 +9,9 @@ import pandas as pd
 
 @login_required
 def ballincollig_properties(request):
-    areas = ['Maglin', 'Old Quarter', 'An Caislean','Muskerry Estate','Greenfield',]
+    areas = ['Maglin', 'Old Quarter', 'An Caislean','Muskerry Estate','Greenfield','Classis Lake',]
     query = 'SELECT * FROM core_ballincolligpropertymodel'
     properties =  BallincolligPropertyModel.objects.raw(query)
-
-
 
     if len(properties) > 0:
         # properties = BallincolligPropertyModel.objects.all().values()
@@ -32,7 +30,7 @@ def ballincollig_properties(request):
 
 @login_required
 def search_property(request):
-    areas = ['Maglin', 'Old Quarter', 'An Caislean', 'Muskerry Estate','Greenfield', ]
+    areas = ['Maglin', 'Old Quarter', 'An Caislean', 'Muskerry Estate','Greenfield','Classis Lake', ]
     area = request.GET['area']
     query = f'SELECT * FROM core_ballincolligpropertymodel WHERE area = "{area}" '
     properties =  BallincolligPropertyModel.objects.raw(query)
@@ -43,3 +41,72 @@ def search_property(request):
     dataset = dataset.drop(["id", "_state"], axis=1)
     dataset = dataset.to_html(index=False)
     return render(request, "ballincollig-properties.html",{"properties": properties, "dataset": dataset,"areas": areas })
+
+
+
+@login_required
+def search_area(request):
+    areas = ['Maglin', 'Old Quarter', 'An Caislean', 'Muskerry Estate', 'Greenfield', 'Classis Lake', ]
+    query = request.GET['sql_query']
+
+    # SELECT * FROM core_ballincolligpropertymodel WHERE area = "Maglin"
+
+    sql_query = f'{query}'
+    sql_query= sql_query.replace( "properties","core_ballincolligpropertymodel")
+
+    if "=" in sql_query and '"' not in sql_query and "AND" not in sql_query:
+        param = sql_query.split("= ")[1].title()
+        sql_query = sql_query.split("=")[0] + '= "' + param + '"'
+        print(sql_query)
+
+    properties = BallincolligPropertyModel.objects.raw(sql_query)
+
+    if len(properties) > 0:
+        # properties = BallincolligPropertyModel.objects.all().values()
+        # Raw SQL Query
+        # dataset = pd.DataFrame(data=properties)
+        dataset = pd.DataFrame([item.__dict__ for item in properties])
+        dataset = dataset.drop(["id", "_state"], axis=1)
+        dataset = dataset.to_html(index=False)
+
+    else:
+        dataset = pd.DataFrame()
+
+    return render(request, "ballincollig-properties.html", {"properties": properties, "dataset":dataset,"areas": areas, })
+
+
+"""
+SEARCH BY YEAR
+"""
+
+
+
+@login_required
+def ballincollig_year(request):
+    years = ['2021', '2020', '2019', '2018', ]
+    query = 'SELECT * FROM core_ballincolligpropertymodel'
+    properties =  BallincolligPropertyModel.objects.raw(query)
+    #properties = BallincolligPropertyModel.objects.all().values()
+    # Raw SQL Query
+    #dataset = pd.DataFrame(data=properties)
+    dataset = pd.DataFrame([item.__dict__ for item in properties])
+    dataset = dataset.drop(["id", "_state"], axis=1)
+    dataset = dataset.to_html(index=False)
+    return render(request, "ballincollig-properties-year.html",{"properties": properties, "dataset": dataset,"years": years })
+
+
+
+@login_required
+def search_year(request):
+    years = ['2021', '2020', '2019', '2018', ]
+    year = request.GET['year']
+    query = f'SELECT * FROM core_ballincolligpropertymodel WHERE sold_date LIKE "{year}%" '
+    properties =  BallincolligPropertyModel.objects.raw(query)
+    #properties = BallincolligPropertyModel.objects.all().values()
+    # Raw SQL Query
+    #dataset = pd.DataFrame(data=properties)
+    dataset = pd.DataFrame([item.__dict__ for item in properties])
+    dataset = dataset.drop(["id", "_state"], axis=1)
+    dataset = dataset.to_html(index=False)
+    return render(request, "ballincollig-properties-year.html",{"properties": properties, "dataset": dataset,"years": years })
+
