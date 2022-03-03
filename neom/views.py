e4ef453043245f255f6ffa3d7e1bf86069c31dee
agatamalucha <agata.malucha@gmail.com>
2022-03-03 19:25:54 +0000

    GET json

--- neom/views.py
+++ neom/views.py
@@ -5,11 +5,16 @@ from django.template.defaulttags import register
 from core.models import BallincolligPropertyModel
 from django.db.models import Sum
 from django.http import HttpResponse
-
 import pandas as pd
 from django.shortcuts import render
+import requests
+import json
 
 
 @login_required
 def neom_home(request):
-    return render(request, "dashboard.html")
+    r = requests.get('https://data-collector.mycaprover.toutf.com/neom/jobs/')
+    print('xxxxxxxxxxxxxxxxxxxxx')
+    print(r.json())
+    return render(request, "dashboard.html",)
+
