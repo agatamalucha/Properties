e9cf1d0559254b3ee8fbc1561c93ae4e0ec8b0de
agatamalucha <agata.malucha@gmail.com>
2022-03-22 19:45:52 +0000

    Search, buttons, average

--- island_property/urls.py
+++ island_property/urls.py
@@ -6,5 +6,6 @@ app_name = "island_property"
 
 urlpatterns = [
     path('', views.island_home, name='island-home'),
+    path('search', views.island_search, name ='island-search')
     ]
 
--- island_property/views.py
+++ island_property/views.py
@@ -17,5 +17,55 @@ def island_home(request):
     txt = r.json()
     data = txt['results']
     df = pd.DataFrame(data)
+    unique_localities =list(df["locality"].unique())
+    advert_type_df = df[df["advert_type"] == "sale"]
+    island_average_sale =advert_type_df.groupby(["island"])["price"].mean()
+    # tenerife_sale_df = df[(df["island"] == "Tenerife")&(df["advert_type"] == "sale")]
+    # tenerife_average_sale = tenerife_sale_df["price"].mean()
+    # print(tenerife_average_sale)
+
+    print(island_average_sale)
+
+    if request.method == "POST":
+        div = request.POST["unique_locality"]
+        df = df[df["locality"] == div]
+        df = df.to_html(index=False)
+        return render(request, "island.html", {"df": df, "unique_localities":unique_localities})
+
     df = df.to_html(index=False)
-    return render(request, "island.html", {"df": df,})
+    return render(request, "island.html", {"df": df, "unique_localities":unique_localities, "island_average_sale":island_average_sale, })
+
+
+@login_required
+def island_search(request):
+    r = requests.get('http://data-collector.mycaprover.toutf.com/island-properties/properties/')
+    txt = r.json()
+    data = txt['results']
+    df = pd.DataFrame(data)
+
+    if request.method == "POST":
+        div = request.POST["search_words"]
+        df = df[df['island'].str.contains(div)]
+        #print(div)
+        df = df.to_html(index=False)
+        return render(request, "island.html", {"df": df })
+    df = df.to_html(index=False)
+    return render(request, "island.html", { "df":df })
+
+
+# @login_required
+# def island_average(request):
+#     r = requests.get('http://data-collector.mycaprover.toutf.com/island-properties/properties/')
+#     txt = r.json()
+#     data = txt['results']
+#     df = pd.DataFrame(data)
+#     unique_localities =list(df["island"].unique())
+#
+#     if request.method == "POST":
+#         div = request.POST["unique_locality"]
+#         df = df[df["locality"] == div]
+#         df = df.to_html(index=False)
+#         return render(request, "island.html", {"df": df, "unique_localities":unique_localities})
+#
+#     df = df.to_html(index=False)
+#     return render(request, "island.html", {"df": df, "unique_localities":unique_localities})
